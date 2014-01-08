4d9a23480b1f8895ac6950474e90f913d05dae09
gordeeva <gordeeva@email.my>
2014-01-08 13:19:32 +0300

    переход с java 1.7 на 1.6

--- src/main/java/com/sam/app/i18n/I18nController.java
+++ src/main/java/com/sam/app/i18n/I18nController.java
@@ -8,7 +8,7 @@ public class I18nController {
 	
 	private static final String BUNDLE_NAME = "com.sam.app.i18n.Messages";
 	
-	private static ConcurrentHashMap<Locale, ResourceBundle> bundles = new ConcurrentHashMap<>(); 
+	private static ConcurrentHashMap<Locale, ResourceBundle> bundles = new ConcurrentHashMap<Locale, ResourceBundle>(); 
 	
 	public static String getString(String name, Locale locale) {		
 		ResourceBundle messages = getBundle(locale);
