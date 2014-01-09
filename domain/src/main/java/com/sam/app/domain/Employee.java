b504b18ab8094423139f4cc1297866641e058401
shumakov <1@2.earth>
2014-01-09 17:01:48 +0300

    - создан тест для hibernate - изменен пароль для бд на "1"

--- domain/src/main/java/com/sam/app/domain/Employee.java
+++ domain/src/main/java/com/sam/app/domain/Employee.java
@@ -1,14 +1,10 @@
 package com.sam.app.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
+//@NamedQuery(name = "getAllEmployees", query = "SELECT e FROM Employee e")
 @Table(name = "employee")
 public final class Employee implements AbstractEntity {
 	
