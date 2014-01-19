a339aca7bf4021ba8671b771f30e256d13b2e6a6
gordeeva <gordeeva@gmail.com>
2014-01-20 00:42:23 +0300

    добавлены ограничения not null для имен департамента, сотрудника и роли добавлен скрипт, содержащий только вставку данных

--- db/companydb data.sql
+++ db/companydb data.sql
@@ -1,67 +1,6 @@
 ﻿SET NAMES 'utf8';
 USE companydb;
 
---
--- Описание для таблицы department
---
-CREATE TABLE department(
-  id INT (11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR (45) NOT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE = INNODB
-AUTO_INCREMENT = 16
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
---
--- Описание для таблицы role
---
-CREATE TABLE role(
-  id INT (11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR (45) NOT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE = INNODB
-AUTO_INCREMENT = 16
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
---
--- Описание для таблицы employee
---
-CREATE TABLE employee(
-  id INT (8) NOT NULL AUTO_INCREMENT,
-  name VARCHAR (30) NOT NULL,
-  department_id INT (11) DEFAULT NULL,
-  PRIMARY KEY (id),
-  INDEX fk_employee_department USING BTREE (department_id),
-  CONSTRAINT fk_employee_department FOREIGN KEY (department_id)
-  REFERENCES department (id)
-)
-ENGINE = INNODB
-AUTO_INCREMENT = 67
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
---
--- Описание для таблицы employee_role
---
-CREATE TABLE employee_role(
-  emp_id INT (11) NOT NULL,
-  role_id INT (11) NOT NULL,
-  roles_order INT (11) NOT NULL,
-  PRIMARY KEY (emp_id, role_id),
-  INDEX role_id_fk_idx USING BTREE (role_id),
-  CONSTRAINT emp_id_fk FOREIGN KEY (emp_id)
-  REFERENCES employee (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT role_id_fk FOREIGN KEY (role_id)
-  REFERENCES role (id) ON DELETE CASCADE ON UPDATE CASCADE
-)
-ENGINE = INNODB
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
 -- 
 -- Вывод данных для таблицы department
 -- 
@@ -70,13 +9,6 @@ INSERT INTO department VALUES(6, 'dep1');
 INSERT INTO department VALUES(11, 'sales dep');
 INSERT INTO department VALUES(14, 'managers dep');
 INSERT INTO department VALUES(15, 'lunatic');
-
-
--- 
--- Вывод данных для таблицы role
--- 
-INSERT INTO role VALUES(7, 'manager');
-INSERT INTO role VALUES(15, 'sales');
 
 
 -- 
@@ -115,3 +47,10 @@ INSERT INTO employee_role VALUES(51, 15, 0);
 INSERT INTO employee_role VALUES(52, 7, 0);
 INSERT INTO employee_role VALUES(53, 15, 0);
 INSERT INTO employee_role VALUES(54, 7, 0);
+
+
+-- 
+-- Вывод данных для таблицы role
+-- 
+INSERT INTO role VALUES(7, 'manager');
+INSERT INTO role VALUES(15, 'sales');
