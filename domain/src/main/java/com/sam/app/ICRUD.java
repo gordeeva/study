7beab23b06b4e16fffed67f1afc1a26b84f2eae7
vladimirshu <1@2.earth>
2014-01-19 13:38:34 +0300

    merge with tomcat-branch

---
+++ domain/src/main/java/com/sam/app/ICRUD.java
@@ -0,0 +1,42 @@
+package com.sam.app;
+
+import java.util.List;
+
+/**
+ * Provides support for Create/Read/Update/Delete operations for any entities
+ * 
+ */
+public interface ICRUD<T> {
+	
+	/**
+	 * Creates new entity
+	 * @param t entity to create
+	 * @return id of new record
+	 */
+	long create(T t);
+	
+	/**
+	 * Returns all entities
+	 * @return entities
+	 */
+	List<T> getAll();
+	
+	/**
+	 * Gets entity
+	 * @param id
+	 * @return entity
+	 */
+	T get(Long id);
+	
+	/**
+	 * Updates entity
+	 * @param entity to update
+	 */
+	T update(T t);
+	
+	/**
+	 * Deletes entity
+	 * @param id
+	 */
+	T delete(long id);
+}
