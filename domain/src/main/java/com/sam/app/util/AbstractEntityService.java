4f276f5a0c7b989f424d1b00e2ea4738210a49bc
gordeeva <gordeeva@gmail.com>
2014-01-20 00:27:04 +0300

    реализована корректная работа с entity manager, управляемым вручную.

--- domain/src/main/java/com/sam/app/util/AbstractEntityService.java
+++ domain/src/main/java/com/sam/app/util/AbstractEntityService.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sam.app.domain.AbstractEntity;
 import com.sam.app.domain.Department;
@@ -19,92 +23,195 @@ import com.sam.app.domain.Role;
 
 public class AbstractEntityService<T extends AbstractEntity> {
 
-	private EntityManagerFactory emf = Persistence
-			.createEntityManagerFactory("study");
-	private EntityManager em = emf.createEntityManager();
+    private static final Logger logger = LoggerFactory
+            .getLogger(AbstractEntityService.class);
 
-	private Class<T> entityClass;
+    private static EntityManagerFactory emf = Persistence
+            .createEntityManagerFactory("study");
 
-	public AbstractEntityService(Class<T> entity) {
-		entityClass = entity;
-	}
+    private EntityManager em;
 
-	public Long save(T entity) {
-		em.getTransaction().begin();
-		em.persist(entity);
-		em.getTransaction().commit();
-		// em.close();
-		// emf.close();
-		return entity.getId();
-	}
-	
-	public T update(T entity) {
-		em.getTransaction().begin();
-		em.merge(entity);
-		em.getTransaction().commit();
-		return entity;
-	}
-	
-	public T delete(T entity) {
-		em.getTransaction().begin();
-		em.remove(entity);
-		em.getTransaction().commit();
-		return entity;
-	}
-	
-	public T delete(Long id) {
-		em.getTransaction().begin();
-		T entity = em.find(entityClass, id);
-		em.remove(entity);
-		em.getTransaction().commit();
-		return entity;
-	}
+    private Class<T> entityClass;
 
-	public T find(Long id) {
-		if (id == null) {
-			throw new IllegalArgumentException();
-		}
-		// em.getTransaction().begin();
+    public AbstractEntityService(Class<T> entity) {
+        entityClass = entity;
+    }
 
-		return em.find(entityClass, id);
-	}
+    public Long save(final T entity) {
+        new EMUtil() {
 
-	public List<T> findAll() {
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<T> cq = cb.createQuery(entityClass);
-		Root<T> root = cq.from(entityClass);
-		cq.select(root);
-		TypedQuery<T> tq = em.createQuery(cq);
-		List<T> result;
-		try {
-			result = tq.getResultList();
-		} catch (NoResultException nre) {
-			result = Collections.emptyList();
-		}
-		return result;
-	}
+            @Override
+            T work() {
+                em.persist(entity);
+                return entity;
+            }
+        }.doWork();
+        return entity.getId();
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<Department> getAllDepartments() {
-		return em.createNamedQuery("all_departments", Department.class)
-				.getResultList();
-	}
+    public T update(final T entity) {
+        @SuppressWarnings("unused")
+        T merged = new EMUtil() {
 
-	@SuppressWarnings("unchecked")
-	public List<Employee> getAllEmployees() {
-		return em.createNamedQuery("all_employees", Employee.class)
-				.getResultList();
-	}
+            @Override
+            T work() {
+                return em.merge(entity);
+            }
+        }.doWork();
 
-    @SuppressWarnings("unchecked")
-	public List<Role> getAllRoles() {
-		return em.createNamedQuery("all_roles", Role.class)
-				.getResultList();
-	}
+        // return merged;
+        return entity;
+    }
 
-	public void close() {
-		em.close();
-		emf.close();
-	}
+    public T delete(final T entity) {
+        new EMUtil() {
 
+            @Override
+            T work() {
+                em.remove(entity);
+                return entity;
+            }
+        }.doWork();
+        return entity;
+    }
+
+    public T delete(final Long id) {
+        T deleted = new EMUtil() {
+
+            @Override
+            T work() {
+                T entity = em.find(entityClass, id);
+                em.remove(entity);
+                return entity;
+            }
+        }.doWork();
+        return deleted;
+    }
+
+    public T find(final Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException();
+        }
+
+        T found = new EMUtil() {
+
+            @Override
+            T work() {
+                return em.find(entityClass, id);
+            }
+        }.doWork();
+        return found;
+    }
+
+    public List<T> findAll() {
+        List<T> list = new EMUtilForList<T>() {
+
+            @Override
+            List<T> work() {
+                CriteriaBuilder cb = em.getCriteriaBuilder();
+                CriteriaQuery<T> cq = cb.createQuery(entityClass);
+                Root<T> root = cq.from(entityClass);
+                cq.select(root);
+                TypedQuery<T> tq = em.createQuery(cq);
+                List<T> result;
+                try {
+                    result = tq.getResultList();
+                } catch (NoResultException nre) {
+                    result = Collections.emptyList();
+                }
+                return result;
+            }
+        }.doWork();
+        return list;
+    }
+
+    public List<Department> getAllDepartments() {
+        List<Department> found = new EMUtilForList<Department>() {
+
+            @Override
+            List<Department> work() {
+                return em.createNamedQuery("all_departments", Department.class)
+                        .getResultList();
+            }
+        }.doWork();
+        return found;
+    }
+
+    public List<Employee> getAllEmployees() {
+        List<Employee> found = new EMUtilForList<Employee>() {
+
+            @Override
+            List<Employee> work() {
+                return em.createNamedQuery("all_employees", Employee.class)
+                        .getResultList();
+            }
+        }.doWork();
+        return found;
+    }
+
+    public List<Role> getAllRoles() {
+        List<Role> found = new EMUtilForList<Role>() {
+
+            @Override
+            List<Role> work() {
+                return em.createNamedQuery("all_roles", Role.class)
+                        .getResultList();
+            }
+        }.doWork();
+        return found;
+    }
+
+    private abstract class EMUtil {
+
+        abstract T work();
+
+        T doWork() {
+            em = emf.createEntityManager();
+            EntityTransaction tx = null;
+            T result = null;
+            try {
+                tx = em.getTransaction();
+                tx.begin();
+
+                result = work();
+
+                tx.commit();
+            } catch (RuntimeException e) {
+                if (tx != null && tx.isActive())
+                    tx.rollback();
+                logger.error("problem at doWork " + e.getMessage());
+                throw e; // or display error message
+            } finally {
+                em.close();
+            }
+            return result;
+        }
+    }
+
+    private abstract class EMUtilForList<E extends AbstractEntity> {
+
+        abstract List<E> work();
+
+        List<E> doWork() {
+            em = emf.createEntityManager();
+            EntityTransaction tx = null;
+            List<E> result = null;
+            try {
+                tx = em.getTransaction();
+                tx.begin();
+
+                result = work();
+
+                tx.commit();
+            } catch (RuntimeException e) {
+                if (tx != null && tx.isActive())
+                    tx.rollback();
+                logger.error("problem at doWorkForList " + e.getMessage());
+                throw e; // or display error message
+            } finally {
+                em.close();
+            }
+            return result;
+        }
+    }
 }
