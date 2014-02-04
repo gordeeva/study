25dcad33c9bc05deb615442ac02c3bf02550412b
shumakov <V.Shumakov@sam-solutions.com>
2014-02-04 14:06:10 +0300

    - Optimized imports

--- domain/src/main/java/com/sam/app/domain/Role.java
+++ domain/src/main/java/com/sam/app/domain/Role.java
@@ -1,10 +1,7 @@
 package com.sam.app.domain;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
--- domain/src/main/java/com/sam/app/util/AbstractEntityService.java
+++ domain/src/main/java/com/sam/app/util/AbstractEntityService.java
@@ -11,13 +11,7 @@ import org.slf4j.LoggerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.Collections;
 import java.util.List;
 
 
